b6e2f302c5957b655a9f865fb6b2aee6ca65ff15
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-08-19 21:14:00 +0200

    onSelect in the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  featureSelected = new EventEmitter<String>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(feature : string) {
+
   }
 
 }
