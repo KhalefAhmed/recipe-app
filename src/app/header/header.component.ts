ea5805a522011cd22b0f201a8d46e9491ae0ca4a
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-08-19 21:48:48 +0200

    methods to navigate

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component, EventEmitter, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  featureSelected = new EventEmitter<String>();
+  @Output() featureSelected = new EventEmitter<String>();
 
   constructor() { }
 
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   }
 
   onSelect(feature : string) {
-
+    this.featureSelected.emit(feature)
   }
 
 }
